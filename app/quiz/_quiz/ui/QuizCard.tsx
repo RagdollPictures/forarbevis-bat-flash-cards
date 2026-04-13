df80769e6e1ac9b0748ac051eeae8d1ede603fc6
ragdoll.pictures <emelie.falk.renstrom@gmail.com>
2026-04-13 12:25:33 +0200

    style problem

--- app/quiz/_quiz/ui/QuizCard.tsx
+++ app/quiz/_quiz/ui/QuizCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, Pressable, Text, View } from "react-native";
-import { styles } from "../styles";
+import { styles } from "../../styles";
 
 export default function QuizCard({
   questionText,
